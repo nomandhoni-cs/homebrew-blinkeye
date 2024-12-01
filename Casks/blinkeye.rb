afaa7e82da8cb3b4bd9aacb08e47e3346e32d904
Noman Dhoni <nomandhoni@yahoo.com>
2024-12-01 21:28:37 +0600

    Updated rb

--- Casks/blinkeye.rb
+++ Casks/blinkeye.rb
@@ -8,7 +8,7 @@ cask "blinkeye" do
   url "https://github.com/nomandhoni-cs/blink-eye/releases/download/v#{version}/Blink.Eye_#{arch}.app.tar.gz",
       verified: "github.com/nomandhoni-cs/blink-eye/"
   name "Blink Eye"
-  desc "Break time reminder app"
+  desc "Blink Eye - Best Eye Care & Break Timer for Mac, Windows, Linux"
   homepage "https://blinkeye.vercel.app/"
 
   auto_updates true
@@ -19,21 +19,25 @@ cask "blinkeye" do
   uninstall quit: "com.blinkeye.app"
 
   caveats <<~EOS
-    ### **Important Notice**
-    Apple requires developers to pay **$100/year** for app notarization. As a small developer, this cost is significant, so this app has not been notarized.
+    IMPORTANT NOTICE:
+
+    Apple requires developers to pay $100/year for app notarization. 
+    
+    As a small developer, this cost is significant, so this app has not been notarized.
 
     As a result, macOS Gatekeeper might block the app. You can bypass this restriction using one of the following methods:
-    
-    1. **Via Finder:**  
-       - Right-click the app in Finder.  
-       - Select **"Open"** to allow it to run.
-    
-    2. **Via Terminal:**  
-       - Run the following command to remove the Gatekeeper quarantine attribute:  
-         ```bash
-         xattr -d com.apple.quarantine /path/to/Blink\ Eye.app
-         ```
+
+    1. Via Finder:
+      - Right-click the app in Finder.
+      - Select "Open" to allow it to run.
+
+    2. Via Terminal:
+      - Run the following command to remove the Gatekeeper quarantine attribute:
+        ```
+        xattr -d com.apple.quarantine /path/to/Blink\ Eye.app
+        ```
 
     Your understanding and support for independent developers like me are greatly appreciated! 💡
   EOS
+
 end
