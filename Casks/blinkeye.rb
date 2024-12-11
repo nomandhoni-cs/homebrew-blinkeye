929c0a55cc050516075c5daa33dc60b7c3d06057
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2024-12-11 23:56:10 +0600

    Update blinkeye.rb

--- Casks/blinkeye.rb
+++ Casks/blinkeye.rb
@@ -1,6 +1,6 @@
 cask "blinkeye" do
   version "2.3.0"
-  sha256 "15893ee25a4f01bac44eda296e6ed16e41964b84a69d88d2581a446b6f66d8aa"
+  sha256 "radmdsa"
 
   url "https://github.com/nomandhoni-cs/blink-eye/releases/download/v#{version}/Blink.Eye_x64.app.tar.gz",
       verified: "github.com/nomandhoni-cs/blink-eye/"
